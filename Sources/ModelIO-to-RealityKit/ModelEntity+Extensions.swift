319f7ef14cdb20f1127f0dde14d69b6d443704aa
radcli14 <radcli14@gmail.com>
2025-12-06 12:14:21 +0100

    added the ModelEntity extension

---
+++ Sources/ModelIO-to-RealityKit/ModelEntity+Extensions.swift
@@ -0,0 +1,22 @@
+//
+//  ModelEntity+Extensions.swift
+//  ModelIO-to-RealityKit
+//
+//  Created by Eliott Radcliffe on 12/6/25.
+//
+
+import Foundation
+import ModelIO
+import RealityKit
+
+extension ModelEntity {
+    /// Create a `ModelEntity` from a `URL` for a file that is of a type supported by `ModelIO`
+    @MainActor
+    static func fromMDLAsset(url: URL) async -> ModelEntity? {
+        // Load the asset using Model I/O.
+        let asset = MDLAsset(url: url)
+        
+        // Asynchronously get its `ModelEntity`
+        return await asset.getModelEntity()
+    }
+}
