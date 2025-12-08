551c7ab39802e423f31b2fafef554225c1d5d84d
radcli14 <radcli14@gmail.com>
2025-12-08 13:53:03 +0100

    added methods to get texture

--- Sources/ModelIO-to-RealityKit/MDLSubmesh+Extensions.swift
+++ Sources/ModelIO-to-RealityKit/MDLSubmesh+Extensions.swift
@@ -8,6 +8,7 @@
 import Foundation
 import ModelIO
 import RealityKit
+import UIKit
 
 /// A single `MDLMesh` can be composed of one or more `MDLSubmesh` instances.
 /// Here is where we find the indices that define face connectivity for the vertices defined at the `MDLMesh` level.
@@ -72,54 +73,83 @@ public extension MDLSubmesh {
     
     // MARK: - Material
     
-    // TODO: Lots here, need to complete the import settings for materials
-    
-    /// Attempts to extract the Base Color, prioritizing texture, then numeric value.
-    @MainActor var pbrBaseColor: PhysicallyBasedMaterial.BaseColor? {
-        let baseColorProperty = material?.property(with: .baseColor)
-        
-        // Check for a texture map (file reference), or a numeric value.
-        if let _ = baseColorProperty?.textureSamplerValue {
-            // TODO: build from texture sampler
-            return nil
-        } else if let color = baseColorProperty?.float4Value {
-            return .init(tint: .init(
+    /// Extract the `UIColor` representation for this material given the specified ModelIO semantic
+    func getColor(mdlSemantic: MDLMaterialSemantic) -> UIColor? {
+        if let material: MDLMaterial,
+            let property: MDLMaterialProperty = material.property(with: mdlSemantic) {
+            let color = property.float4Value
+            return .init(
                 red: CGFloat(color[0]),
                 green: CGFloat(color[1]),
                 blue: CGFloat(color[2]),
                 alpha: CGFloat(color[3])
-            ))
+            )
         }
         return nil
     }
     
-    @MainActor var pbrNormal: PhysicallyBasedMaterial.Normal? {
+    /// Get a texture resource representing an image, using ModelIO semantic to unpack from the `material.propery`, and RealityKit semantic to initialize the `TextureResource`.
+    @MainActor func getTextureResource(
+        mdlSemantic: MDLMaterialSemantic,
+        rkSemantic: TextureResource.Semantic
+    ) -> TextureResource? {
+        if let material: MDLMaterial,
+            let property: MDLMaterialProperty = material.property(with: mdlSemantic),
+            let sampler: MDLTextureSampler = property.textureSamplerValue,
+            let texture: MDLTexture = sampler.texture,
+            let image: CGImage = texture.imageFromTexture()?.takeRetainedValue(),
+            let resource = try? TextureResource(image: image, options: .init(semantic: rkSemantic)) {
+            
+            // Successfully obtained the resource, return it
+            return resource
+        }
         return nil
     }
     
+    // TODO: Lots here, need to complete the import settings for materials
+    
+    /// Attempts to extract the Base Color, prioritizing texture, then numeric value.
+    @MainActor var pbrBaseColor: PhysicallyBasedMaterial.BaseColor? {
+        // Check for a texture map (file reference), or a numeric value.
+        if let resource = getTextureResource(mdlSemantic: .baseColor, rkSemantic: .color) {
+            return .init(texture: .init(resource))
+            
+        // Otherwise check for the constant color
+        } else if let color = getColor(mdlSemantic: .baseColor) {
+            return .init(tint: color)
+        }
+        return nil
+    }
+    
+    /// Create the PBR material's normal image
+    @MainActor var pbrNormal: PhysicallyBasedMaterial.Normal? {
+        if let resource = getTextureResource(mdlSemantic: .tangentSpaceNormal, rkSemantic: .normal) {
+            return .init(texture: .init(resource))
+        }
+        return nil
+    }
+    
+    /// Create the PBR material's roughness image or float value
     @MainActor var pbrRoughness: PhysicallyBasedMaterial.Roughness? {
         // TODO: add some better logic for when to use roughness vs specular, I'm using it the way I am here because it seemed lost in the Blender .obj file export
         let roughnessProperty = material?.property(with: .roughness)
         let specularProperty = material?.property(with: .specularExponent)
         if let value = specularProperty?.floatValue {
-            let convertedRoughness = sqrt(2.0 / (value + 2.0))
-            print("specularExponent: \(value), convertedRoughness: \(convertedRoughness)")
-            return .init(floatLiteral: convertedRoughness)
-        } else if let _ = roughnessProperty?.textureSamplerValue {
-            // TODO: build from texture sampler
+            return .init(floatLiteral: sqrt(2.0 / (value + 2.0)))
+        } else if let resource = getTextureResource(mdlSemantic: .roughness, rkSemantic: .raw) {
+            return .init(texture: .init(resource))
         } else if let value = roughnessProperty?.floatValue {
-            print("roughness: \(value)")
             return .init(floatLiteral: value)
         }
         return nil
     }
     
+    /// Create the PBR material's metallic image or float value
     @MainActor var pbrMetallic: PhysicallyBasedMaterial.Metallic? {
         let metallicProperty = material?.property(with: .metallic)
-        if let _ = metallicProperty?.textureSamplerValue {
-            // TODO: build from texture sampler
+        if let resource = getTextureResource(mdlSemantic: .metallic, rkSemantic: .raw) {
+            return .init(texture: .init(resource))
         } else if let value = metallicProperty?.floatValue {
-            print("metallic: \(value)")
             return .init(floatLiteral: value)
         }
         return nil
